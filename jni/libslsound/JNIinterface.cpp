35db22c65c67e4ebfe7919f9de57b4e254e33a09
Yoshihito Kira <bigbros@madtape.com>
2014-10-17 22:32:42 +0900

    JNI_OnLoad() にて、エラー時に  JNI_ERRを返すよう修正。

--- jni/libslsound/JNIinterface.cpp
+++ jni/libslsound/JNIinterface.cpp
@@ -10,7 +10,7 @@ JNI_OnLoad(JavaVM *vm, void *reserved)
 {
     JNIEnv *env;
     if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
-        return JNI_VERSION_1_6;
+        return JNI_ERR;
     }
     return JNI_VERSION_1_6;
 }
